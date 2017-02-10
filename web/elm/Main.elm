4a4696b62e06987f26e3b702b09ff555164a7986
Will Strinz <wstrinz@gmail.com>
2017-02-10 11:55:02 -0600

    WIP load json feeds from server

--- web/elm/Main.elm
+++ web/elm/Main.elm
@@ -1,6 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (div, text)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Json
 
 
 type alias Model =
@@ -9,7 +12,8 @@ type alias Model =
 
 type Msg
     = Noop
-    | LoadStories
+    | LoadStory (Result Http.Error String)
+    | FetchStory
 
 
 initialModel : Model
@@ -17,7 +21,7 @@ initialModel =
     { title = "A story", author = "Me", content = "this is some story content" }
 
 
-main : Program Never Model msg
+main : Program Never Model Msg
 main =
     Html.program
         { init = ( initialModel, Cmd.none )
@@ -27,15 +31,46 @@ main =
         }
 
 
-update : a -> b -> ( b, Cmd msg )
+getStory : Cmd Msg
+getStory =
+    let
+        storyUrl =
+            "/stories/1"
+
+        req =
+            Http.get storyUrl decodeStoryData
+    in
+        Http.send LoadStory req
+
+
+decodeStoryData : Json.Decoder String
+decodeStoryData =
+    Json.at [ "data", "title" ] Json.string
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        FetchStory ->
+            ( model, getStory )
+
+        LoadStory (Ok storyData) ->
+            ( { model | title = storyData }
+            , Cmd.none
+            )
+
+        LoadStory (Err _) ->
+            ( model, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
 
 
-view : Model -> Html.Html msg
+view : Model -> Html.Html Msg
 view model =
     div []
         [ Html.h2 [] [ text model.title ]
         , Html.h4 [] [ text model.author ]
         , Html.p [] [ text model.content ]
+        , Html.button [ onClick FetchStory ] [ text "load" ]
         ]
