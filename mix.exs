dd9c2469603d396b914d5dd7c00268f1d49bf509
Will Strinz <wstrinz@gmail.com>
2017-05-22 16:46:51 -0500

    version bamp

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BSB.Mixfile do
 
   def project do
     [app: :bsb,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
