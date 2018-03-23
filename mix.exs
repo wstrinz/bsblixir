c460582c1636e4f7896eb66fd52f8feaf6fde12a
Will Strinz <wstrinz@gmail.com>
2018-03-23 11:48:40 -0500

    try setting version manually

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BSB.Mixfile do
   def project do
     [
       app: :bsb,
-      version: "0.0.#{committed_at()}",
+      version: "0.1.0",
       elixir: "~> 1.6.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
