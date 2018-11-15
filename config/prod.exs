f2d06578c29117859944befeb801f738024f1de3
Will Strinz <wstrinz@gmail.com>
2018-11-14 20:56:36 -0600

    version to make heroku happy

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,7 @@ config :bsb, BSB.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   root: ".",
   server: true,
-  version: Mix.Project.config()[:version],
+  version: "1.0",
   code_reloader: false,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
